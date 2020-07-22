bc95657b80146879ce3e15099e44629b170e4086
kazikamrulislam <kamrul.lab@gmail.com>
2020-07-22 14:34:18 +0600

    first commit

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+function openNav(){
+    document.getElementById("N_sidenav").style.width = "200px";
+    document.getElementById("N_sidenav").style.display = "block";
+}
+
+function closeNav(){
+    document.getElementById("N_sidenav").style.width = "0";
+    document.getElementById("N_sidenav").style.display = "block";
+}
+
+document.getElementById("N_sidenav").style.display = "none";
